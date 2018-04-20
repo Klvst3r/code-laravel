8530277571e398e8bcfe509718ee0681ad632b44
klvst3r <klvst3r@gmail.com>
2018-04-20 13:05:36 -0500

    Add migrations

---
+++ database/migrations/2018_04_19_174511_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("nombre",50);
+            $table->text("detalle");
+            $table->double("precio");
+            $table->integer("cantidad");
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
